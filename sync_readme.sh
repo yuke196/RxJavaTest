c93a131b32de961a8095a3723eaf2cefef189923
yuke <363564023@qq.com>
2014-09-28 10:47:37 +0800

    update

--- sync_readme.sh
+++ sync_readme.sh
@@ -3,4 +3,3 @@ cat ~/git/RxJavaTest/head >  ~/git/RxJavaTest/README.md
 cat ~/git/RxJavaTest/app/src/main/java/com/sharyuke/rxjavatest/MyActivity.java >> ~/git/RxJavaTest/README.md
 cat ~/git/RxJavaTest/foot  >>  ~/git/RxJavaTest/README.md
 git add ~/git/RxJavaTest/README.md
-git commit -m 'update readme'
